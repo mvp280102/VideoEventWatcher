9db3a79bb985a325e42592a813c70c86d5f3e515
mvp280102 <mvp280102@gmail.com>
2023-12-25 18:45:19 +0500

    Move sender to class.

--- sender.py
+++ sender.py
@@ -1,13 +1,22 @@
 from pika import BlockingConnection, ConnectionParameters, BasicProperties, DeliveryMode
+from utils import create_logger
 
 
-def send_events(events):
-    connection = BlockingConnection(ConnectionParameters('localhost'))
-    channel = connection.channel()
-    channel.queue_declare(queue='vew_events', durable=True)
+# TODO: Logger.
+class EventSender:
+    logger = create_logger(__name__)
 
-    for event in events:
-        channel.basic_publish(exchange='', routing_key='vew_events', body=str(event),
-                              properties=BasicProperties(delivery_mode=DeliveryMode.Persistent))
+    def __init__(self, queue_name, host_name='localhost'):
+        self.queue_name = queue_name
+        self.host_name = host_name
 
-    connection.close()
+    def send_events(self, events):
+        connection = BlockingConnection(ConnectionParameters(self.host_name))
+        channel = connection.channel()
+        channel.queue_declare(queue=self.queue_name, durable=True)
+
+        for event in events:
+            channel.basic_publish(exchange='', routing_key=self.queue_name, body=str(event),
+                                  properties=BasicProperties(delivery_mode=DeliveryMode.Persistent))
+
+        connection.close()
