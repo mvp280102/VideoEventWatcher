8d1f97846fd259f3048dd453983b65a0cea01ef9
mvp280102 <mvp280102@gmail.com>
2023-12-26 18:58:04 +0500

    Add events saving to database.

--- saver.py
+++ saver.py
@@ -1,8 +1,10 @@
+import json
 import numpy
 
 from sqlalchemy import create_engine, Column, ARRAY, INTEGER, VARCHAR, TIMESTAMP
 from sqlalchemy.orm import DeclarativeBase, Session
 
+from pika import BlockingConnection, ConnectionParameters
 from psycopg2.extensions import register_adapter, AsIs
 
 
@@ -24,18 +26,32 @@ class Event(BaseModel):
 
 # TODO: Logger.
 class EventSaver:
-    def __init__(self, database_url, path, events):
+    def __init__(self, database_url, queue_name, host_name='localhost'):
         self.engine = create_engine(database_url)
         BaseModel.metadata.create_all(bind=self.engine)
         register_adapter(numpy.int64, lambda int64: AsIs(int64))
 
-        self.path = path
-        self.raw_events = events
+        self.queue_name = queue_name
+        self.host_name = host_name
 
-    def save_events(self):
-        with Session(autoflush=False, bind=self.engine) as session:
-            for raw_event in self.raw_events:
-                raw_event['filename'] = self.path
+    def save_events(self, filename):
+        connection = BlockingConnection(ConnectionParameters(host=self.host_name))
+        channel = connection.channel()
+        channel.queue_declare(queue=self.queue_name, durable=True)
+
+        while True:
+            method_frame, _, body = channel.basic_get(queue=self.queue_name)
+
+            if not method_frame:
+                break
+
+            with Session(autoflush=False, bind=self.engine) as session:
+                raw_event = json.loads(body)
+                raw_event['filename'] = filename
                 event = Event(**raw_event)
                 session.add(event)
                 session.commit()
+
+            channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+
+        connection.close()
