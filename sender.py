4a9b6cc769bd95e3ddf6173ded5e0d82428eed12
mvp280102 <mvp280102@gmail.com>
2023-12-26 15:53:15 +0500

    Add message body json serialization.

--- sender.py
+++ sender.py
@@ -1,11 +1,10 @@
+import json
+
 from pika import BlockingConnection, ConnectionParameters, BasicProperties, DeliveryMode
-from utils import create_logger
 
 
 # TODO: Logger.
 class EventSender:
-    logger = create_logger(__name__)
-
     def __init__(self, queue_name, host_name='localhost'):
         self.queue_name = queue_name
         self.host_name = host_name
@@ -16,7 +15,7 @@ class EventSender:
         channel.queue_declare(queue=self.queue_name, durable=True)
 
         for event in events:
-            channel.basic_publish(exchange='', routing_key=self.queue_name, body=str(event),
+            channel.basic_publish(exchange='', routing_key=self.queue_name, body=json.dumps(event),
                                   properties=BasicProperties(delivery_mode=DeliveryMode.Persistent))
 
         connection.close()
