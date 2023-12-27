eed7bc1a564b8b9c27978187f6207c99f7516a11
mvp280102 <mvp280102@gmail.com>
2023-12-27 13:23:41 +0500

    Add logger.

--- sender.py
+++ sender.py
@@ -2,19 +2,26 @@ import json
 
 from pika import BlockingConnection, ConnectionParameters, BasicProperties, DeliveryMode
 
+from utils import create_logger
 
-# TODO: Logger.
+
 class EventSender:
+    logger = create_logger(__name__)
+
     def __init__(self, queue_name, host_name='localhost'):
         self.queue_name = queue_name
         self.host_name = host_name
 
     def send_events(self, events):
+        if not events:
+            return
+
         connection = BlockingConnection(ConnectionParameters(self.host_name))
         channel = connection.channel()
         channel.queue_declare(queue=self.queue_name, durable=True)
 
         for event in events:
+            self.logger.info(f'Send event message {event} to message queue.')
             channel.basic_publish(exchange='', routing_key=self.queue_name, body=json.dumps(event),
                                   properties=BasicProperties(delivery_mode=DeliveryMode.Persistent))
 
