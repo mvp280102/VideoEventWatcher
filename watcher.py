68d2eab1d09014adb0ba05cee0e09df7a41e49e1
mvp280102 <mvp280102@gmail.com>
2024-01-05 15:26:35 +0500

    Refactor instance vars.

--- watcher.py
+++ watcher.py
@@ -16,10 +16,7 @@ class EventWatcher:
 
     def __init__(self, config):
         self.config = config
-
         self.reader, self.writer = None, None
-        self.total_frames, self.fps = None, None
-        self.frame_width, self.frame_height = None, None
 
     async def watch_events(self, file_name):
         input_path = join(self.config.inputs_root, file_name)
@@ -27,22 +24,22 @@ class EventWatcher:
 
         self.reader = cv2.VideoCapture(str(input_path))
 
-        self.total_frames = int(self.reader.get(cv2.CAP_PROP_FRAME_COUNT) / 2)
-        self.fps = int(self.reader.get(cv2.CAP_PROP_FPS))
+        total_frames = int(self.reader.get(cv2.CAP_PROP_FRAME_COUNT) / 2)
+        fps = int(self.reader.get(cv2.CAP_PROP_FPS))
 
-        self.frame_width = int(self.reader.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.frame_height = int(self.reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        frame_width = int(self.reader.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(self.reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        frame_size = (frame_width, frame_height)
 
         fourcc = cv2.VideoWriter.fourcc(*'XVID')
 
-        self.writer = cv2.VideoWriter(filename=str(output_path), fourcc=fourcc, fps=self.fps,
-                                      frameSize=(self.frame_width, self.frame_height))
+        self.writer = cv2.VideoWriter(filename=str(output_path), fourcc=fourcc, fps=fps, frameSize=frame_size)
 
         line_data = self.config.line_data if 'line_data' in self.config else None
 
-        processor = FrameProcessor(self.config.processor, (self.frame_width, self.frame_height), line_data)
-        visualizer = EventVisualizer((self.frame_width, self.frame_height), line_data)
-        extractor = EventExtractor(self.config.extractor, input_path)
+        processor = FrameProcessor(self.config.processor, frame_size, line_data)
+        visualizer = EventVisualizer(frame_size, line_data)
+        extractor = EventExtractor(self.config.extractor, input_path, fourcc, fps, frame_size)
         sender = EventSender(self.config.sender, input_path)
 
         total_stats = defaultdict(lambda: 0)
@@ -53,7 +50,7 @@ class EventWatcher:
             if self.config.frames_skip and index % (self.config.frames_skip + 1):
                 continue
 
-            self.logger.debug("Processing frame {} of {}...".format(index, self.total_frames))
+            self.logger.debug("Processing frame {} of {}...".format(index, total_frames))
             start = time.time()
 
             tracks = await processor.get_tracks(frame, self.config.filter_label)
